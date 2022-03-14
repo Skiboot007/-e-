d03f85e694afd9be9e9f3ca5ea8482aa61944e59
Skiboot007 <64629929+Skiboot007@users.noreply.github.com>
2022-03-14 13:50:35 +0000

    Update e(hub)main.lua

--- e(hub)main.lua
+++ e(hub)main.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
 if game.PlaceId == 4620170611 then
-    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Skiboot007/-e-/main/e(hub)Breakin.lua"))()
 elseif game.PlaceId == 155615604 then
     local venyx = library.new("E(hub) Prison Life", 5013109572)
     local p1 = venyx:addPage("Main", 5012544693)
