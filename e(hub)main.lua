3cb77ede2b5c97bc9832c68c075d1dda592b8fab
Skiboot007 <64629929+Skiboot007@users.noreply.github.com>
2022-03-14 14:07:14 +0000

    Update e(hub)main.lua

--- e(hub)main.lua
+++ e(hub)main.lua
@@ -1,4 +1,3 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
 if game.PlaceId == 4620170611 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Skiboot007/-e-/main/e(hub)Breakin.lua"))()
 elseif game.PlaceId == 155615604 then
