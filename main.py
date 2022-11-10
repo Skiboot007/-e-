30d7d58c39172fe8ce3e3a71715671f947cbe211
Skiboot007 <64629929+Skiboot007@users.noreply.github.com>
2022-11-10 20:39:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import os
-import random
-import time
-f = open('main.py','r')
-a = f.read()
-f.close()
-input('do u want to continue')
-c = str(random.choice(os.listdir(os.getcwd()))+'\\'+'main'+str(random.randint(1, 100000))+'.py')
-print(c)
-f = open(c,'w')
-f.write('''import time
-import os
-while True:
-  time.sleep(1)
-  os.system("cmd")''')
-f.close()
-os.system(f'py {c}')
+print("e")
