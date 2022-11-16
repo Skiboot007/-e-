3eeb2c590ab49ba5800c73e0fcf650397c37bd59
Skiboot007 <64629929+Skiboot007@users.noreply.github.com>
2022-11-16 02:03:54 +0000

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-ie = true
-while (ie = true) {
-    await new Promise(r => setTimeout(r, 1))
-    document.querySelector("#__next > div.layout_layout__yqn2P > div > div.layout_layoutColumn__I6TSP > div.game-layout_gameLayout__bhg7L > div.game-layout_gameLayoutColumn__KpZFV.game-layout_gameLayoutColumnLeft__EwPIu > div > button").click()
-}
