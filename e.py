fd70a8dbc145d490383896553ba3a1bb1887aced
Skiboot007 <64629929+Skiboot007@users.noreply.github.com>
2023-01-07 03:52:14 +0000

    Update e.py

--- e.py
+++ e.py
@@ -1,15 +1,21 @@
 import socket
-server = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
-server.bind(("2a00:4802:d0e:6200:44ef:d8d9:f112:f703",9191))
-server.listen()
+import colorama
+colorama.init()
+ip = "192.158.0.34"
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect((ip,9999))
+print(colorama.Fore.LIGHTCYAN_EX+"""
+ _______  __   __  _______  _______  _______    _______  _______  ___      _______  ___   _______ 
+|       ||  | |  ||       ||       ||       |  |       ||       ||   |    |       ||   | |       |
+|    ___||  |_|  ||   _   ||  _____||_     _|  |  _____||    _  ||   |    |   _   ||   | |_     _|
+|   | __ |       ||  | |  || |_____   |   |    | |_____ |   |_| ||   |    |  | |  ||   |   |   |  
+|   ||  ||       ||  |_|  ||_____  |  |   |    |_____  ||    ___||   |___ |  |_|  ||   |   |   |  
+|   |_| ||   _   ||       | _____| |  |   |     _____| ||   |    |       ||       ||   |   |   |  
+|_______||__| |__||_______||_______|  |___|    |_______||___|    |_______||_______||___|   |___|                                                                                          
+""")
 while True:
-    try:
-        client,addr = server.accept()
-        while True:
-            command=client.recv(1024).decode()
-            if command == "idk":
-                client.send("idk ehter".encode())
-            else:
-                print(command)
-    except ConnectionResetError:
-        continue
+    print(f"{colorama.Fore.LIGHTMAGENTA_EX}[{colorama.Fore.LIGHTGREEN_EX}+{colorama.Fore.LIGHTMAGENTA_EX}] {colorama.Fore.LIGHTCYAN_EX}please input command",end="")
+    k=input("=>")
+    if k=="idk":
+        client.send(k.encode())
+        print(client.recv(1024).decode())
